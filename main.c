140dca2ece892cafa9290d48d81317f105d9e042
SIGMazer <mazinasd7@gmail.com>
2023-10-21 16:55:33 +0300

    tokanizer implementaion

--- main.c
+++ main.c
@@ -1,131 +1,144 @@
+#include <stdint.h>
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <assert.h>
 #include <stdlib.h>
 #include <ctype.h>
+#include <stdbool.h>
 #include <string.h>
 
-#define TEXT_CAP 10000
+#define TEXT_CAP 1024 
+#define TOKEN_CAP 256
+#define PUTBACK_LEN 32
 #define ARRLEN(arr) (sizeof arr / sizeof arr[0])
 
 
-char text[TEXT_CAP] = {0};
-size_t text_i = 0;
-
-
 typedef struct {
-    char token[256];
-}token;
+    char contant[TEXT_CAP];
+    size_t idx;
+} Text;
 
-static int next(void){
-    return text[text_i++];
-}
+typedef struct{
+    unsigned char token[TOKEN_CAP];
+    size_t idx;
+} Token;
 
-static int skip(void){
-    uint8_t c;
-    c = next();
-    while(c >= 127 || isspace(c))
-        c= next();
-
-    return c;
-}
-static void scantoken(uint8_t c, char *buf){
-    size_t i = 0 ;
-    while(isspace(c))
-        c = next();
-    while((isdigit(c) || isalpha(c))){
-        if(256 < i){
-            fprintf(stderr,"Token too long\n");
-            exit(1);
-        }else{
-            buf[i++] = c;
-        }
-
-        c = next();
-    }
-    buf[i] = '\0';
-}
-
-
-void parse_xhtml(FILE *file, char *text){
+Text text = {.idx = 0};
+size_t clen = 0;
+int parse_xhtml(FILE *file){
     char c;
-    int i = 0;
+    bool in_tag = false;
+    bool left_space = true;
+    bool end_tag = false;
     while((c = fgetc(file)) != EOF){
         if(c == '<'){
-            while((c = fgetc(file)) != '>'){
-                if(c == EOF){
-                    break;
-                }
+            in_tag = true;
+        }
+        else if(c == '/'){
+            end_tag = true;
+        }
+        else if(c == '>'){
+            in_tag = false;
+            if (end_tag){
+                text.contant[text.idx]= '\0';
+                return 0; // return 0 if end tag
             }
         }
-        else{
-            text[i] = c;
-            i++;
+        else if(!in_tag){
+            if(!isspace(c) || !left_space){
+                if(left_space) left_space = false;
+                
+                text.contant[text.idx]= c;
+                assert(text.idx < TEXT_CAP);
+                text.idx++;
+            }
         }
     }
-    text[i] = '\0';
+    return EOF; // return EOF if no more char
 }
 
-void parse_xhtml_trim(FILE *file, char *text){
-    char c;
-    int i = 0;
-    int space = 0;
-    int empty = 0;
-    while((c = fgetc(file)) != EOF){
-        if(c == '<'){
-            while((c = fgetc(file)) != '>'){
-                if(c == EOF){
-                    break;
-                }
-            }
-        }
-        else{
-            if(c == '\n'){
-                if(empty == 0){
-                    text[i] = c;
-                    i++;
-                    empty = 1;
-                }
-            }
-            else if(c == ' '){
-                if(space == 0){
-                    text[i] = c;
-                    i++;
-                    space = 1;
-                }
-            }
-            else{
-                text[i] = c;
-                i++;
-                space = 0;
-                empty = 0;
-            }
-        }
-    }
-    text[i] = '\0';
+int read_next_tag(FILE *file){
+    int c = 0;
+    do{
+        c = parse_xhtml(file);
+    }while(c != EOF && text.idx == 0);
+    return c; // if no more tags 
 }
 
-void token_dump(){
-    uint8_t c  = 0;
-    char buf[256] ;
-    size_t t = ARRLEN(text);
-    while(text_i < t){
-        c = skip();
-        scantoken(c, buf);
-        puts(buf);
-        memset(buf, 0, 256);
+char putback[PUTBACK_LEN] = {0};
+size_t putback_idx = 0;
+
+Token read_alpha(){
+    int c = text.contant[clen];
+    Token token = {.idx = 0};
+    while(isalpha(c) || isdigit(c) || c == '_'){
+        token.token[token.idx] = c;
+        assert(token.idx < TOKEN_CAP);
+        token.idx++;
+        clen++;
+        c = text.contant[clen];
     }
+    return token;
+}
+
+Token read_number(){
+    int c = text.contant[clen];
+    Token token = {.idx = 0};
+    while(isdigit(c)){
+        token.token[token.idx] = c;
+        assert(token.idx < TOKEN_CAP);
+        token.idx++;
+        clen++;
+        c = text.contant[clen];
+        if(c == '.'){
+            if(!isdigit(text.contant[clen+1]))
+                continue;
+            token.token[token.idx] = c;
+            assert(token.idx < TOKEN_CAP);
+            token.idx++;
+            clen++;
+            c = text.contant[clen];
+        }
+    }
+    return token;
+}
+
+Token lexer(){
+    uint8_t c= text.contant[clen]; 
+    Token token = {.idx =0};
+
+    while(isspace(c)){
+        clen++;
+        c = text.contant[clen];
+    }
+
+    if(isalpha(c)) token = read_alpha();
+    else if(isdigit(c)) token = read_number();
+    else {
+        token.token[token.idx] = c;
+        assert(token.idx < TOKEN_CAP);
+        token.idx++;
+        clen++;
+    }
+
+    return token;
 }
 
 
 int main(void){
-
     FILE *file = fopen("./docs.gl/el3/cos.xhtml", "r");  
+    int c; 
+    while((c = read_next_tag(file)) != EOF){
+        Token token = lexer();
+        printf("%s\n", token.token);
+        memset(token.token, 0, sizeof token.token);
+        memset(text.contant, 0, sizeof text.contant);
+        token.idx = 0;
+        text.idx = 0;
+        clen = 0;
+    }
 
-    parse_xhtml_trim(file, text);
-    
-    token_dump();
-
-
+    fclose(file);
     return 0;
 }
